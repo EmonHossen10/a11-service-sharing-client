cba8fd7eb9a6270091225b6af4249fcd4f5361fa
unknown <emonhossen1000@gmail.com>
2023-11-08 15:19:58 +0600

    router setup and css library setup

---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,11 @@
+import Home from "../Pages/Home";
+
+const Layout = () => {
+  return (
+    <div>
+      <Home></Home>
+    </div>
+  );
+};
+
+export default Layout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+ 
+import "./index.css";
+import "./app.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./Layout/Layout.jsx";
+import Home from "./Pages/Home.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    children:[
+      {
+        path:"/",
+        element:<Home></Home>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
