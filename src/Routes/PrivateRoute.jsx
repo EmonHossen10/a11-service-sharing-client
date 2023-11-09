c9fc0ad605da6ca6008bcfc564552802be63ddc8
unknown <emonhossen1000@gmail.com>
2023-11-09 16:44:24 +0600

    private route loading added

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,11 +1,28 @@
- 
+/* eslint-disable react/prop-types */
+import { useContext } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
+import { Navigate, useLocation } from "react-router-dom";
 
-const PrivateRoute = () => {
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+  console.log(location.pathname);
+
+  //
+
+  if (loading) {
     return (
-        <div>
-            
-        </div>
+      <div className="flex  mt-52 justify-center  items-center">
+        <span className="loading loading-spinner text-error loading-lg"></span>
+      </div>
     );
+  }
+
+  if (user) {
+    return children;
+  }
+
+  return <Navigate state={location.pathname} to="/login"></Navigate>;
 };
 
 export default PrivateRoute;
