d1b8774b8d49d7a83a89ed62ff74e489a718e677
unknown <emonhossen1000@gmail.com>
2023-11-16 14:38:59 +0600

    take your plan added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,19 +2,20 @@ import Footer from "../Shared/Footer";
 import Navbar from "../Shared/Navbar";
 import Banner from "./Banner";
 import Faq from "./Faq";
+
 import PopularServices from "./PopularServices";
-
- 
-
+import Pricing from "./Pricing";
 
 const Home = () => {
   return (
-    <div  >
+    <div>
       <nav>
         <Navbar></Navbar>
         <Banner></Banner>
         <PopularServices></PopularServices>
         <Faq></Faq>
+        <Pricing></Pricing>
+
         <Footer></Footer>
       </nav>
     </div>
